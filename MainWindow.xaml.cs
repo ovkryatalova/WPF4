1e597a6501b775e9e7b49058fbd25e18061369a5
Ольга Кряталова <olyakryat@mail.ru>
2021-10-09 00:43:42 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            NewWindow window = new NewWindow("Модальное");
+            window.ShowDialog();
+            // Код после метода ShowDialog выполнится только тогда, кода диалоговое окно закроется.
+        }
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            NewWindow window = new NewWindow("Немодальное");
+            window.Show();
+            // Код после метода Show выполнится сразу.
+        }
+    }
+}
---
+++ NewWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF4
+{
+    /// <summary>
+    /// Логика взаимодействия для NewWindow.xaml
+    /// </summary>
+    public partial class NewWindow : Window
+    {
+        public NewWindow()
+        {
+            InitializeComponent();
+        }
+        public NewWindow(string title) //: this()
+        {
+            InitializeComponent(); // Переопределяя конструктор мы должны вызывать метод InitializeComponent
+            Title = title;
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
